ad2a334337bd37f60ea47990ef50987d699b7771
Jin <it.minhhung@gmail.com>
2020-11-21 17:28:48 +0700

    - allow change side-bar style - checkbox using fontawesome and so on...

--- SerenitySkins/SerenitySkins.Web/Modules/Common/Helpers/J.initSkins.ts
+++ SerenitySkins/SerenitySkins.Web/Modules/Common/Helpers/J.initSkins.ts
@@ -3,12 +3,10 @@
     export function initSkins(themeCookieName: string) {
 
         let curSkinClass = $.cookie(themeCookieName) || "blue";
-        switch (curSkinClass.toLowerCase().trim()) {
-            case "dark-001":
-                Serenity.DataGrid.defaultRowHeight = 30;
-                Serenity.DataGrid.defaultHeaderHeight = 30;
+        console.log(curSkinClass);
 
-                break;
+        switch (curSkinClass.toLowerCase().trim()) {
+            
             case "glass-001-style-1":
             case "glass-001-style-2":
             case "glass-001-style-3":
@@ -18,17 +16,6 @@
             case "glass-001-style-7":
             case "glass-001-style-8":
             case "glass-001-style-9":
-            case "glass-001-style-10":
-            case "glass-001-style-11":
-            case "glass-001-style-12":
-            case "glass-001-style-13":
-            case "glass-001-style-14":
-            case "glass-001-style-15":
-            case "glass-001-style-16":
-            case "glass-001-style-17":
-            case "glass-001-style-18":
-            case "glass-001-style-19":
-            case "glass-001-style-20":
 
                 $.datepicker.setDefaults({
                     buttonImage: null,
